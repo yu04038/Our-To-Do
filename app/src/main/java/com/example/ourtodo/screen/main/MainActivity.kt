6b1b675cc171a9664cdaee065e091cd7aa71516e
yu04038 <yu04038@gmail.com>
2022-03-31 20:14:45 +0900

    create: 프로젝트 생성

--- app/src/main/java/com/example/ourtodo/screen/main/MainActivity.kt
+++ app/src/main/java/com/example/ourtodo/screen/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.ourtodo
+package com.example.ourtodo.screen.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ourtodo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
