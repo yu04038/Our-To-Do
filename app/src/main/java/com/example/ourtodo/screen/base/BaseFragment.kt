e37f28be90c5c92702b5bc29bf572788148863ed
yu04038 <yu04038@gmail.com>
2022-04-02 02:05:57 +0900

    feat: BaseActivity, BaseFragment, BaseViewModel 생성

---
+++ app/src/main/java/com/example/ourtodo/screen/base/BaseFragment.kt
@@ -0,0 +1,55 @@
+package com.example.ourtodo.screen.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+import com.example.ourtodo.viewmodel.BaseViewModel
+import kotlinx.coroutines.Job
+
+abstract class BaseFragment<VM: BaseViewModel, VB: ViewBinding>: Fragment() {
+
+    abstract val viewModel: VM
+
+    protected lateinit var binding: VB
+
+    abstract fun getViewBinding(): VB
+
+    private lateinit var fetchJob: Job
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = getViewBinding()
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initState()
+    }
+
+    open fun initState() {
+        arguments?.let {
+            viewModel.storeState(it)
+        }
+        initViews()
+        fetchJob = viewModel.fetchData()
+        observeData()
+    }
+
+    open fun initViews() = Unit
+
+    abstract fun observeData()
+
+    override fun onDestroy() {
+        if (fetchJob.isActive) {
+            fetchJob.cancel()
+        }
+        super.onDestroy()
+    }
+}
