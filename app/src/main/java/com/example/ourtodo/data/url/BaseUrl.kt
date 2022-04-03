02e1dab004c2844d507e3adcd60eb3f467882ae7
yu04038 <yu04038@gmail.com>
2022-04-03 20:51:00 +0900

    feat: 이메일 입력 기능 구현

---
+++ app/src/main/java/com/example/ourtodo/data/url/BaseUrl.kt
@@ -0,0 +1,5 @@
+package com.example.ourtodo.data.url
+
+object BaseUrl {
+    const val BASE_URL = "http://3.37.137.217:8080/"
+}
--- app/src/main/java/com/example/ourtodo/screen/login/EmailCertificateFragment.kt
+++ app/src/main/java/com/example/ourtodo/screen/login/EmailCertificateFragment.kt
@@ -1,5 +1,11 @@
 package com.example.ourtodo.screen.login
 
+import android.graphics.Color
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import com.example.ourtodo.R
 import com.example.ourtodo.databinding.FragmentEmailCertificateBinding
 import com.example.ourtodo.screen.base.BaseFragment
 import com.example.ourtodo.viewmodel.EmailCertificateViewModel
@@ -11,11 +17,46 @@ class EmailCertificateFragment : BaseFragment<EmailCertificateViewModel, Fragmen
 
     override val viewModel by viewModel<EmailCertificateViewModel>()
 
+    private var clickable = false
+
     override fun observeData() {
     }
 
     override fun initViews() = with(binding) {
         super.initViews()
+
+        goToCertification.setOnClickListener {
+            if (clickable) {
+                Log.e("goToCertification", "클릭")
+            }
+        }
+
+        emailCertification.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                val email = p0.toString()
+                val isValid = android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+                if (!isValid) {
+                    emailCertification.setBackgroundResource(R.drawable.email_certificate_edittext_shape_not_valid)
+                    goToCertification.setBackgroundResource(R.drawable.email_certificate_button_shape_disabled)
+                    goToCertification.setTextColor(Color.parseColor("#BFBFBF"))
+                    clickable = false
+                    warning.visibility = View.VISIBLE
+                } else {
+                    emailCertification.setBackgroundResource(R.drawable.email_certificate_edittext_shape_valid)
+                    goToCertification.setBackgroundResource(R.drawable.email_certificate_button_shape_enabled)
+                    goToCertification.setTextColor(Color.parseColor("#333333"))
+                    clickable = true
+                    warning.visibility = View.INVISIBLE
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+        })
     }
 
     companion object {
