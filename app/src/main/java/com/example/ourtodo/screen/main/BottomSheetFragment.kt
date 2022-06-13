e865b5f940e841283ad5e8342ede8d5189eebdd2
yu04038 <yu04038@gmail.com>
2022-06-13 20:57:44 +0900

    feat: color picker

--- app/src/main/java/com/example/ourtodo/screen/main/BottomSheetFragment.kt
+++ app/src/main/java/com/example/ourtodo/screen/main/BottomSheetFragment.kt
@@ -5,10 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.example.ourtodo.R
+import com.example.ourtodo.databinding.DialogAddTodoBinding
+import com.example.ourtodo.databinding.FragmentBottomSheetBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class BottomSheetFragment : BottomSheetDialogFragment() {
+
+    private lateinit var binding: FragmentBottomSheetBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,5 +21,37 @@ class BottomSheetFragment : BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_bottom_sheet, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) = with(binding) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tagXButton.setOnClickListener {
+            dialog?.dismiss()
+        }
+
+        colorPickerRed.setOnClickListener {
+            dialog?.dismiss()
+        }
+
+        colorPickerRed.setOnClickListener {
+            dialog?.dismiss()
+        }
+
+        colorPickerRed.setOnClickListener {
+            dialog?.dismiss()
+        }
+
+        colorPickerRed.setOnClickListener {
+            dialog?.dismiss()
+        }
+
+        colorPickerRed.setOnClickListener {
+            dialog?.dismiss()
+        }
+
+        colorPickerRed.setOnClickListener {
+            dialog?.dismiss()
+        }
     }
 }
